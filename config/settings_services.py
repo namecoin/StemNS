ac67ad03a7ffc7211a5df56d82daff359825559f
Jeremy Rand <jeremyrand@danwin1210.de>
2022-05-10 01:56:53 +0000

    Remove leading underscore from config names
    They were confusing, and date back to the TorNS era.

--- config/settings_services.py
+++ config/settings_services.py
@@ -2,7 +2,7 @@ import os
 from os.path import join, split
 import sys
 
-_service_to_command = {
+service_to_command = {
     "pet.onion": [
         sys.executable,
         join(split(__file__)[0], 'ns_petname.py'),
@@ -14,10 +14,10 @@ _service_to_command = {
 }
 
 
-def _bootstrap_callback():
+def bootstrap_callback():
     pass
 
 
-def _exit_callback():
+def exit_callback():
     # Can't use sys.exit() here because it's called from a child thread.
     os._exit(0)
